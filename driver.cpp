52e28acbc7a9ce17fceb6d4723b140664600504d
Che Kofif <github@gmmorris.com>
2011-07-11 01:02:51 +0100

    built structure for linked pointer Skip list

---
+++ driver.cpp
@@ -0,0 +1,32 @@
+
+
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include "LinkedPtrSkipList.hpp"
+
+class MyObj
+{
+	public:
+	int ID;
+};
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] )
+{
+	LinkedPtrSkipList<MyObj>* list = new LinkedPtrSkipList<MyObj>();
+	MyObj* obj = new MyObj();
+	obj->ID = 1;
+	string keyA = "A";
+	//list->Find(keyA);
+	list->Insert(keyA,obj);
+	
+	
+	
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
